a8c8ebc7f64b4a601f7e4fb5ed0923be9e3f795d
Manolitoxx1 <ojosparami@gmail.com>
2026-09-16 17:18:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker para Buen Día Café - Club de Fidelización
-const CACHE_NAME = 'buendia-fidelizacion-v2.0.1';
+const CACHE_NAME = 'buendia-fidelizacion-v2.0.2';
 const ASSETS = [
   './',
   './index.html',
